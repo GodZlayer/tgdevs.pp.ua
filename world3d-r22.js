ee57fbbfe63212c63e5831ed9f18e58a8283242a
GodZlayer <santoss.cog@gmail.com>
2026-09-21 08:19:46 -0300

    r25: use real 3D TGDesk comet blade system

--- world3d-r22.js
+++ world3d-r22.js
@@ -15,7 +15,7 @@ import {
   setTGDeskSystemOpacity,
   createPremiumWordmarkR22,
   updatePremiumWordmarkR22
-} from './r24/tgdesk-system.js';
+} from './r25/tgdesk-system.js';
 
 export class TGWorld3D extends TGWorld3DBase{
   constructor(canvas){

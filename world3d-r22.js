682c01a77b57c1162a58711c84c7f2ef4989bac4
GodZlayer <santoss.cog@gmail.com>
2026-09-21 08:03:46 -0300

    r24: route TGDesk animation through canonical comet trails

--- world3d-r22.js
+++ world3d-r22.js
@@ -15,7 +15,7 @@ import {
   setTGDeskSystemOpacity,
   createPremiumWordmarkR22,
   updatePremiumWordmarkR22
-} from './r22/tgdesk-system.js';
+} from './r24/tgdesk-system.js';
 
 export class TGWorld3D extends TGWorld3DBase{
   constructor(canvas){
